9ca3c3403bb8bf9dd0403c285bb9c36a93928851
Jenia Dysin <genia90@gmail.com>
2019-07-12 23:28:53 +0300

    Finished initial objects and functions.

---
+++ src/defs.h
@@ -0,0 +1,34 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#include <Arduino.h>
+#include <NeoPixelBus.h>
+
+
+#define SUCCESS_CODE 0
+#define FAIL_CODE -1
+
+
+typedef struct {
+    const uint16_t PixelCount;
+    const uint8_t Pin;
+    const String name;
+    const unsigned long irCode;
+    NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> *strip;
+} Area; 
+
+#define ALL_AREAS_CODE 0xFD00FF
+#define AREA_ONE_CODE 0xFD08F7
+#define AREA_TWO_CODE 0xFD8877
+#define AREA_THREE_CODE 0xFD48B7
+#define AREA_FOUR_CODE 0xFD28D7
+#define AREA_FIVE_CODE 0xFDA857
+#define BLANK_CODE 0xFFFFFFFF
+
+
+int decodeRemoteCommands(const unsigned long *data);
+int activateArea(const Area *area);
+int deactivateArea(const Area *area);
+int setArea(const Area *area, RgbColor color);
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,22 @@
-#include <Arduino.h>
+#include "defs.h"
 #include <NeoPixelBus.h>
 #include <IRremote.h>
 
-#define SUCCESS_CODE 0
-#define FAIL_CODE -1
+#define NUM_OF_AREAS 5
 
-const uint16_t AreaOnePixelCount = 5;
-const uint8_t AreaOnePixelPin = 5;
-
-// Uncomment for more strips
-// const uint16_t AreaTwoPixelCount = 6;
-// const uint8_t AreaTwoPixelPin = 5;
-// const uint16_t AreaThreePixelCount = 7;
-// const uint8_t AreaThreePixelPin = 5;
-// const uint16_t AreaFourPixelCount = 8;
-// const uint8_t AreaFourPixelPin = 5;
-// const uint16_t AreaFivePixelCount = 9;
-// const uint8_t AreaFivePixelPin = 5;
-
-NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> stripAreaOne(AreaOnePixelCount, AreaOnePixelPin);
-// NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> stripAreaTwo(AreaTwoPixelCount, AreaTwoPixelPin);
-// NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> stripAreaThree(AreaThreePixelCount, AreaThreePixelPin);
-// NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> stripAreaFour(AreaFourPixelCount, AreaFourPixelPin);
-// NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> stripAreaFive(AreaFivePixelCount, AreaFivePixelPin);
+// Define areas
+const Area areas[NUM_OF_AREAS] = {
+    {5, 5, "Area 1", AREA_ONE_CODE, new NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod>(5, 5)},
+    {5, 6, "Area 2", AREA_TWO_CODE, new NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod>(5, 6)},
+    {5, 7, "Area 3", AREA_THREE_CODE, new NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod>(5, 7)},
+    {5, 8, "Area 4", AREA_FOUR_CODE, new NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod>(5, 8)},
+    {5, 9, "Area 5", AREA_FIVE_CODE, new NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod>(5, 9)},
+};
 
 // IR setup
 const int IR_PIN = 12;
 IRrecv recv(IR_PIN);
 decode_results irData;
-
-// Function declarations
-int decodeRemoteCommands(unsigned long data);
 
 void setup()
 {
@@ -39,21 +25,17 @@ void setup()
     Serial.println("Initializing...");
     Serial.flush();
 
+    // Enable IR sensor
     Serial.println("Starting IR sensor...");
     recv.enableIRIn();
     Serial.println("IR enabled.");
 
     // Initialize led strips
-    stripAreaOne.Begin();
-    stripAreaOne.Show();
-    // stripAreaTwo.Begin();
-    // stripAreaTwo.Show();
-    // stripAreaThree.Begin();
-    // stripAreaThree.Show();
-    // stripAreaFour.Begin();
-    // stripAreaFour.Show();
-    // stripAreaFive.Begin();
-    // stripAreaFive.Show();
+    for(int i = 0; i < NUM_OF_AREAS; i++)
+    {
+        areas[i].strip->Begin();
+        areas[i].strip->Show();
+    }
 
     Serial.println();
     Serial.println("Running...");
@@ -64,9 +46,9 @@ void loop()
     if(recv.decode(&irData))
     {
         // Skip if button pressed longer than needed.
-        if(irData.value != 0xFFFFFFFF)
+        if(irData.value != BLANK_CODE)
         {
-            if(decodeRemoteCommands(irData.value) == FAIL_CODE)
+            if(decodeRemoteCommands(&irData.value) == FAIL_CODE)
             {
                 Serial.print("Code 0x");
                 Serial.print(irData.value, HEX);
@@ -82,32 +64,40 @@ void loop()
 /**
  * Activates areas based on the keys pressed.
  */
-int decodeRemoteCommands(unsigned long data)
+int decodeRemoteCommands(const unsigned long *data)
 {
-    switch (data)
+    switch (*data)
     {
-        case 0xFD00FF: // Power Button
+        case ALL_AREAS_CODE: // Power Button
             Serial.println("Activating all areas...");
+            for(int i = 0; i < NUM_OF_AREAS; i++)
+                activateArea(&areas[i]);
             return SUCCESS_CODE;
 
-        case 0xFD08F7: // "1" Button
+        case AREA_ONE_CODE: // "1" Button
+            if((areas[1].strip)->IsDirty());
             Serial.println("Activating area 1...");
+            activateArea(&areas[0]);
             return SUCCESS_CODE;
 
-        case 0xFD8877: // "2" Button
+        case AREA_TWO_CODE: // "2" Button
             Serial.println("Activating area 2...");
+            activateArea(&areas[1]);
             return SUCCESS_CODE;
 
-        case 0xFD48B7: // "3" Button
+        case AREA_THREE_CODE: // "3" Button
             Serial.println("Activating area 3...");
+            activateArea(&areas[2]);
             return SUCCESS_CODE;
 
-        case 0xFD28D7: // "4" Button
+        case AREA_FOUR_CODE: // "4" Button
             Serial.println("Activating area 4...");
+            activateArea(&areas[3]);
             return SUCCESS_CODE;
 
-        case 0xFDA857: // "5" Button
+        case AREA_FIVE_CODE: // "5" Button
             Serial.println("Activating area 5...");
+            activateArea(&areas[4]);
             return SUCCESS_CODE;
         
         default:
@@ -117,7 +107,31 @@ int decodeRemoteCommands(unsigned long data)
     return FAIL_CODE;
 }
 
-int activateArea(unsigned short area)
+/**
+ * Set all pixels in an area to white color
+ */
+int activateArea(const Area *area)
 {
+    Serial.print("Setting ");
+    Serial.print(area->name);
+    Serial.println(" to white");
+    return setArea(area, RgbColor(255));
+}
+
+/**
+ * Sets all pixels in an area to black
+ */
+int deactivateArea(const Area *area)
+{
+    return setArea(area, RgbColor(0));
+}
+
+/**
+ * Set all pixels in an area to specific color
+ */
+int setArea(const Area *area, RgbColor color)
+{
+    area->strip->ClearTo(color);
+    area->strip->Show();
     return SUCCESS_CODE;
 }
